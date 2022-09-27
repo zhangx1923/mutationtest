aa1764b1059144c1c22905aa8d75a044fa52d5e6
zhangx1923 <xz8@email.sc.edu>
2022-09-26 22:47:35 -0400

    update

---
+++ job.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python train.py --batch-size 32 --test-batch-size 1000 --epochs 20 --lr 0.1 --gamma 0.7 --cuda True --gpu 5 | tee b32e20l01g07.txt
+python train.py --batch-size 32 --test-batch-size 1000 --epochs 20 --lr 1 --gamma 0.7 --cuda True --gpu 5 | tee b32e20l1g07.txt
+python train.py --batch-size 64 --test-batch-size 1000 --epochs 20 --lr 0.1 --gamma 0.7 --cuda True --gpu 5 | tee b64e20l01g07.txt
+python train.py --batch-size 64 --test-batch-size 1000 --epochs 20 --lr 1 --gamma 0.7 --cuda True --gpu 5 | tee b64e20l1g07.txt
