bf0149649f36b62105d9aa0272d64c9ddfe053ad
zhangx1923 <xz8@email.sc.edu>
2022-09-27 20:14:10 -0400

    up

--- job.sh
+++ job.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --batch-size 32 --test-batch-size 1000 --epochs 20 --lr 0.1 --gamma 0.7 --device cuda --gpu 5 --evaluate eva| tee b32e20l01g07.txt
+python train.py --batch-size 32 --test-batch-size 1000 --epochs 20 --lr 0.1 --gamma 0.7 --device cuda --gpu 5 --evaluate eva| tee mu.txt
