807c478273c381c01c3163a574a893c00364a09b
zhangx1923 <xz8@email.sc.edu>
2022-11-01 01:06:47 -0400

    update

--- model.py
+++ model.py
@@ -210,7 +210,7 @@ class Net(nn.Module):
                 index_row = torch.tensor(index_row)
                 index_col = torch.tensor(index_col)
                 instance[ind][index_row, index_col] = tar[index_row, index_col]
-                print(block_row_count, block_col_count,start_row,start_col,instance[ind][index_row, index_col])
+                #print(block_row_count, block_col_count,start_row,start_col,instance[ind][index_row, index_col])
         return x
 
     def forward(self, x):
