5e748d2442c1554d8bd5018bbdb63d634658943d
somnussyq <somnussyq@163.com>
2022-11-29 14:56:44 -0500

    update

--- jobcifar_train.sh
+++ jobcifar_train.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --batch-size 32 --test-batch-size 1000 --epochs 20 --lr 0.1 --gamma 0.7 --device cuda --gpu 7 --evaluate train --dataset cifar| tee cifar_train.txt
+python train.py --batch-size 32 --test-batch-size 1000 --epochs 20 --lr 0.1 --gamma 0.7 --device cuda --gpu 7 --evaluate train --dataset cifar --model 3 --mutationType r --rmp 2| tee cifar2_train.txt
