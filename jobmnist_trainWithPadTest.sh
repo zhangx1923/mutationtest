2ea890d3022768b7d44559bdf19dd2d1d0ed1652
somnussyq <somnussyq@163.com>
2022-11-29 18:54:40 -0500

    update

--- jobmnist_trainWithPadTest.sh
+++ jobmnist_trainWithPadTest.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-python train.py --batch-size 32 --test-batch-size 1000 --model 1 --padtest 1 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 2 --evaluate train --dataset mnist --mutationType r --rmp 2| tee mnist_trainWithPad.txt
+python train.py --batch-size 32 --test-batch-size 1000 --model 1 --padtest 1 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 2 --evaluate train --dataset mnist --mutationType r --rmp 2| tee mnist_trainWithPad2.txt
+
+python train.py --batch-size 32 --test-batch-size 1000 --model 1 --padtest 1 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 2 --evaluate train --dataset mnist --mutationType r --rmp 3| tee mnist_trainWithPad3.txt
+
+python train.py --batch-size 32 --test-batch-size 1000 --model 1 --padtest 1 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 2 --evaluate train --dataset mnist --mutationType r --rmp 4| tee mnist_trainWithPad4.txt
+
+python train.py --batch-size 32 --test-batch-size 1000 --model 1 --padtest 1 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 2 --evaluate train --dataset mnist --mutationType r --rmp 5| tee mnist_trainWithPad5.txt
