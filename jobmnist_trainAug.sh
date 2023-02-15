585014fee01207e1bbff847c91592d16c81c2a25
zhangx1923 <xz8@email.sc.edu>
2023-02-15 12:35:43 -0500

    up

--- jobmnist_trainAug.sh
+++ jobmnist_trainAug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python train.py --batch-size 32 --test-batch-size 1000 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 5 --evaluate train --dataset mnist --model 1 --mutationType r --rmp 3 --padtest 1 --aug 7| tee mnist13_trainAugNew7.txt
+python train.py --batch-size 32 --test-batch-size 1000 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 4 --evaluate train --dataset mnist --model 1 --mutationType r --rmp 3 --padtest 1 --aug 7| tee mnist13_trainAugNew7.txt
 # python train.py --batch-size 32 --test-batch-size 1000 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 5 --evaluate train --dataset mnist --model 1 --mutationType r --rmp 3 --padtest 1 --aug 7| tee mnist13_trainAug7.txt
 # python train.py --batch-size 32 --test-batch-size 1000 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 5 --evaluate train --dataset mnist --model 1 --mutationType r --rmp 4 --padtest 1 --aug 7| tee mnist14_trainAug7.txt
 # python train.py --batch-size 32 --test-batch-size 1000 --epochs 10 --lr 0.1 --gamma 0.7 --device cuda --gpu 5 --evaluate train --dataset mnist --model 1 --mutationType r --rmp 5 --padtest 1 --aug 7| tee mnist15_trainAug7.txt
